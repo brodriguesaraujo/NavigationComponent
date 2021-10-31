7edb123e3a9727852028ab1f6da5b99082fceda8
Bruno Rodrigues <brunorodriguesdearaujo1@gmail.com>
2021-10-31 12:30:26 -0300

    create classes StartFragment and ProfileFragment

--- app/src/main/java/br/com/navigationcomponent/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/navigationcomponent/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.navigationcomponent
+package br.com.navigationcomponent.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.navigationcomponent.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
