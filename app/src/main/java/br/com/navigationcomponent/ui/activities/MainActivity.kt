0f9f7bb14f42e7e81c1dbf89f65b6a22d485818a
Bruno Rodrigues <brunorodriguesdearaujo1@gmail.com>
2021-10-31 15:11:51 -0300

    set toolbar

--- app/src/main/java/br/com/navigationcomponent/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/navigationcomponent/ui/activities/MainActivity.kt
@@ -2,11 +2,34 @@ package br.com.navigationcomponent.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import br.com.navigationcomponent.R
+import br.com.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.myToolbar)
+
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) ||super.onSupportNavigateUp()
     }
 }
