8bd770a1386d9941aa4455e94785afab73e4cf24
Bruno Rodrigues <brunorodriguesdearaujo1@gmail.com>
2021-10-31 15:12:39 -0300

    set navigation for other fragment

--- app/src/main/java/br/com/navigationcomponent/ui/fragments/StartFragment.kt
+++ app/src/main/java/br/com/navigationcomponent/ui/fragments/StartFragment.kt
@@ -5,14 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.navigationcomponent.R
+import br.com.navigationcomponent.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
+
+    private lateinit var binding: FragmentStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    ): View {
+        binding = FragmentStartBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonNext.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_profileFragment)
+        }
     }
 }
